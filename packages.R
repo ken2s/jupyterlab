0d8f363055709dfedee29115e265ea0a5b4941de
ken2s <ken2.sugimoto@gmail.com>
2023-06-04 23:04:29 +0900

    modify cran repo

--- packages.R
+++ packages.R
@@ -1,4 +1,4 @@
 targetPackages <- c('ISLR', 'car', 'fmsb', 'class', 'boot', 'leaps', 'glmnet', 'pls', 'splines', 'locfit', 'gam', 'akima', 'tree', 'randomForest', 'gbm', 'e1071', 'LiblineaR', 'ROCR', 'openxlsx', 'PerformanceAnalytics', 'abind', 'bitops', 'readr', 'dplyr', 'ggplot2', 'forcats', 'tidyverse', 'multcomp', 'mvtnorm', 'TH.data', 'sandwich', 'xgboost', 'kernlab') 
 newPackages <- targetPackages[!(targetPackages %in% installed.packages()[,"Package"])]
-if(length(newPackages)) install.packages(newPackages, repos = "https://cran.ism.ac.jp")
+if(length(newPackages)) install.packages(newPackages, repos = "https://ftp.yz.yamagata-u.ac.jp/pub/cran/")
 for(package in targetPackages) library(package, character.only = T)
